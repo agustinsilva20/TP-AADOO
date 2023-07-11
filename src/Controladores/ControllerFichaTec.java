8ffeb9e0c88f5c40390595b4b49553e4064ff2c7
Juanni1 <juanignacioalippi@gmail.com>
2023-07-11 18:53:14 -0300

    Entrega Final TPO

--- src/Controladores/ControllerFichaTec.java
+++ src/Controladores/ControllerFichaTec.java
@@ -1,8 +1,11 @@
 package Controladores;
 
 import Clases.Animal.Animal;
+import Clases.Control.Control;
+import Clases.Encuesta.Visita;
 import Clases.Exportador.IEstrategiaExportador;
 import Clases.FichaTecnica.FichaTecnica;
+import Clases.TratamientoMedico.TratamientoMed;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +31,18 @@ public class ControllerFichaTec {
 
     public void crearFichaTec(Animal animal, IEstrategiaExportador exportador){
         fichasTecnicas.add(new FichaTecnica(animal,exportador));
+    }
+
+    public void agregarVisitaFichaTec(FichaTecnica fichaTecnica, Visita visita){
+        fichaTecnica.agregarVisita(visita);
+    }
+
+    public void agregarControlFichaTec(FichaTecnica fichaTecnica, Control contol){
+        fichaTecnica.agregarControl(contol);
+    }
+
+    public void agregarTratamiendoMedFichaTec(FichaTecnica fichaTecnica, TratamientoMed tratamientoMed){
+        fichaTecnica.agregarTratamiento(tratamientoMed);
     }
 
     public List<FichaTecnica> getFichasTecnicas(){
